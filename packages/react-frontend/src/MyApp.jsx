e559f14d8f71d9f43061df2820691306d6fa5fa6
Gary Chiu <gchiu118@gmail.com>
2026-09-02 16:14:57 -0700

    feat: added CORS

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,16 +1,7 @@
-// src/MyApp.jsx
-import React, { useState } from "react";
+import React, {useState, useEffect} from 'react';
 import Table from "./Table";
-import Form from "./Form";
 
-function MyApp() {
-
-  const [characters, setCharacters] = useState([]);
-
-  function updateList(person) {
-    setCharacters([...characters, person]);
-  }
-
+const [characters, setCharacters] = useState([]);
   function removeOneCharacter(index) {
     const updated = characters.filter((character, i) => {
       return i !== index;
@@ -18,16 +9,26 @@ function MyApp() {
     setCharacters(updated);
   }
 
-  return (
-    <div className="container">
-      <Table
-        characterData={characters}
-        removeCharacter={removeOneCharacter}
-      />
-      <Form handleSubmit={updateList} />
-    </div>
-  );
+function fetchUsers() {
+    const promise = fetch("http://localhost:8000/users");
+    return promise;
 }
+
+useEffect(() => {
+  fetchUsers()
+	  .then((res) => res.json())
+	  .then((json) => setCharacters(json["users_list"]))
+	  .catch((error) => { console.log(error); });
+}, [] );
+
+return (
+  <div className="container">
+    <Table
+      characterData={characters}
+      removeCharacter={removeOneCharacter}
+    />
+  </div>
+);
 
 
 export default MyApp;
